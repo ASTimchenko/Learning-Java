ad999940b0643bd9a35643c777aedb6f5111b69c
Андрей <nbvtim80@gmail.com>
2021-11-21 00:19:39 +0700

    Решение квадратного уравнения и вычисление факториала числа с использованием ввода чисел

---
+++ src/main/java/lesson5/FibonacciSequence.java
@@ -0,0 +1,26 @@
+package lesson5;
+
+import java.util.Scanner;
+
+public class FibonacciSequence {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите номер n-го члена последовательности Фибоначчи: ");
+        int number = scanner.nextInt();
+        System.out.println(fibonacciCalculate(number));
+    }
+
+    public static int fibonacciCalculate(int number){
+        //Первое число последовательности Фибоначчи = 0.
+        if(number == 1){
+            return 0;
+        }
+        //Второе число последовательности Фибоначчи = 1.
+        if(number == 2){
+            return 1;
+        }
+        //Вычисление других членов последовательности Фибоначчи. Их номер не меньше 3.
+        return fibonacciCalculate(number-2) + fibonacciCalculate(number-1);
+    }
+}
